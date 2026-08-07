f4983beee8ef3231ec6c4b1adc7865ca3eca2a4e
Sana Begum <shehzadi664@gmail.com>
2026-08-07 22:09:10 +0530

    Updated Python programs and renamed file

--- Practice.py
+++
@@ -1,7 +0,0 @@
-num1= 10
-num2= 30
-num3 = 40
-
-print("Sum =", num1+num2+num3, "is:", sum)
-
-print("hello world")
---
+++ Python prgs.py
@@ -0,0 +1,23 @@
+#  Program using Variables 
+a = 10
+b = 20
+c = a + b
+print(" sum =",c)
+
+#program using  loop
+for i in range ( 0,12):
+    print(i)
+
+# program using function
+def add(a,b):
+    print("sum =",a+b)
+add ( 20, 76)
+
+# program using list
+A = [ 5 , 15 , 25 , 35 , 45]
+print ("List =", A)
+print("Largest number =",max(A))
+print ("Smallest number =", min(A))
+print("Sum of A =", sum(A))
+print ("Average of A = ", sum(A)/len(A))
+
